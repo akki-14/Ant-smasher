acb0320d3520ae003050d146c26b8bcd5b78c4ba
akki-14 <akki@Akhil-2.local>
2015-06-12 05:12:08 +0530

    icon change
    and server control for rate app

--- rateThisApp.lua
+++ rateThisApp.lua
@@ -1,13 +1,53 @@
 
 module( ..., package.seeall )
+local json = require("json")
 
 function rateThis()
     local thisRate = ice:loadBox( "myRate" );
     local time = thisRate:retrieve( "time" );
-    -- IF YOU RESET THE CODE UNCOMMENT THE CODE BELLOW
-    --thisRate:store("time",nil);
-    --thisRate:save();
-    if time == nil  then
+    local currentVersion = thisRate:retrieve( "popup_version" );
+    local previousVersion = thisRate:retrieve( "popup_previous_version" );
+    
+    network.request( "http://gaak.atwebpages.com/rate_popup.php", "GET", function(event)
+        print ( "call back"  )
+        if ( event.isError ) then
+            print( "Network error!")
+        else
+            local t = json.decode( event.response )
+            if previousVersion == nil or t.popup_version > previousVersion then
+                currentVersion = t.popup_version
+                thisRate:store("popup_version",currentVersion);
+                thisRate:save();
+                print("version", currentVersion)
+            end
+            
+        end
+    end
+    )
+    local function votarandroid( event )
+        if "clicked" == event.action then
+            local i = event.index
+            if 1 == i then
+                Analytics.logEvent("rate_app_click")
+                time = 5
+                thisRate:store("time",time);
+                thisRate:save();
+                system.openURL("market://details?id=" .. system.getInfo("androidAppPackageName") )
+            elseif 2 == i then
+                time = -5
+                thisRate:store("popup_previous_version",currentVersion);
+                thisRate:store("time",time);
+                thisRate:save();
+            end
+        end
+    end
+    
+    if currentVersion ~= nil and previousVersion ~= nil and currentVersion ~= previousVersion and time ~= 5 then
+        Analytics.logEvent("rate_app_shown")
+        native.showAlert("ENJOYING The Game!!" , "How would you rate Ant Smasher",
+        { "Rate 5 stars", "Later" }, votarandroid )
+        
+    elseif time == nil  then
         -- FIRST TIME ENTERING YOUR APP
         time =1;
         thisRate:store("time",time);
@@ -16,56 +56,17 @@ function rateThis()
     elseif time == 5 then
         -- THE USER DO NOT WANT TO RATE YOUR APP OR HE ALREADY MADE
         return true;
-    elseif time < 1 then
-        -- IF TIME == 2 W8 FOR THE NEXT TIME
+    elseif time < 4 then
         time = time+1
         print(time)
         thisRate:store("time",time);
         thisRate:save();
         return true; 
-    elseif time == 1 then
-        -- IF TIME == 2 W8 FOR THE NEXT TIME
-        time = time+1
-        thisRate:store("time",time);
-        thisRate:save();
-        return true;
-    elseif time == 2 then
-        -- IF TIME == 2 W8 FOR THE NEXT TIME
-        time = time+1
-        thisRate:store("time",time);
-        thisRate:save();
-        return true;
-    elseif time == 3 then
-        -- IF TIME == 3 W8 FOR THE NEXT TIME
-        time = time+1
-        thisRate:store("time",time);
-        thisRate:save();
-        return true;
     elseif time == 4 then
-        -- IF TIME == 3 CALL THE VOTE;
-        -- EVENT NATIVE ALERT FOR ANDROID
-        local function votarandroid( event )
-            if "clicked" == event.action then
-                local i = event.index
-                if 1 == i then
-                    Analytics.logEvent("rate_app_click")
-                    system.openURL("market://details?id=" .. system.getInfo("androidAppPackageName") )
-                    time = 5
-                    thisRate:store("time",time);
-                    thisRate:save();
-                elseif 2 == i then
-                    time = -2
-                    thisRate:store("time",time);
-                    thisRate:save();
-                end
-            end
-        end
         Analytics.logEvent("rate_app_shown")
         native.showAlert("ENJOYING The Game!!" , "How would you rate Ant Smasher",
         { "Rate 5 stars", "Later" }, votarandroid )
         
     end
-    
-    
     
 end
